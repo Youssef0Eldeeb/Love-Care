5155ee60c620ca0009a443169c05d4706c7c3bbd
Youssef0Eldeeb <youssefeldeeb3109@gmail.com>
2022-10-24 14:16:47 +0200

    Initial Commit

---
+++ Love&Care/Love_CareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Love_CareApp.swift
+//  Love&Care
+//
+//  Created by Youssef Eldeeb on 24/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Love_CareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
